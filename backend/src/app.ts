dff85abedf85aff1681abd803a1e0d7104f6bbf3
msmaiaa <roxx77344@gmail.com>
2021-07-16 19:18:01 -0300

    (back)feat: add user routes on express router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import ProductRoutes from './routes/ProductRoutes'
+import User from './routes/UserRoutes'
 
 dotenv.config()
 
@@ -10,5 +11,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', ProductRoutes)
+app.use('/auth/user', User)
 
 export default app
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,4 @@
 import app from './app'
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000, () => {
+  console.log('App listening')
+})
