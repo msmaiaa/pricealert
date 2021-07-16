eca34988da362e2fd21f74bb12f1a06f2d59cfa1
msmaiaa <roxx77344@gmail.com>
2021-07-16 20:25:55 -0300

    (back)fix: fix mongodb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,10 @@ dotenv.config()
 import app from './app'
 import mongoose from 'mongoose'
 
-mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true }, () =>{
+mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
   console.log('Connected to the database')
-})
-app.listen(process.env.PORT || 5000, () => {
-  console.log('App listening')
+  app.listen(process.env.PORT || 5000, () => {
+    console.log('App listening')
+  })
 })
