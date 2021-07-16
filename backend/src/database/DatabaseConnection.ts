0ab46cc9ad7be7ba970d2b902bfdc01f3bcc5fe8
msmaiaa <roxx77344@gmail.com>
2021-07-16 19:21:36 -0300

    (back)refactor: move mongoose connection to the app start file

--- backend/src/database/DatabaseConnection.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import mongoose from 'mongoose'
-
-mongoose.connect(`${process.env.MONGO_URI}`, () =>{
-  console.log('Connected to the database')
-})
-
-export default mongoose
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import app from './app'
+import mongoose from 'mongoose'
+
+mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true }, () =>{
+  console.log('Connected to the database')
+})
 app.listen(process.env.PORT || 5000, () => {
   console.log('App listening')
 })
