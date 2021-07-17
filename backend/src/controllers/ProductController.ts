b85d8c31ef73320fe9b5edec7cd9c123a3bd0233
msmaiaa <roxx77344@gmail.com>
2021-07-17 17:30:23 -0300

    (back)feat: get all products for the given users working

--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from 'express'
+import ProductsRepository from '../repositories/ProductsRepository'
 
 export default new class ProductController {
-  async index(req: Request,res: Response): Promise<any> {
-    return res.status(200).send({message: 'index'})
+  async index(req: any, res: Response) {
+    try{
+      const userid = req.userid
+      const productsInDB = await ProductsRepository.findAll(userid)
+      return res.status(200).send({ message: 'User products fetched with success', body: productsInDB })
+    }catch(e) {
+      console.error(e)
+      return res.status(404).send({ message: 'Error while trying to fetch user products' })
+    }
   }
 
-  async post(req: Request,res: Response): Promise<any> {
+  async post(req: Request, res: Response) {
     return res.status(200).send({message: 'post'})
   }
 
-  async put(req: Request,res: Response): Promise<any> {
+  async put(req: Request, res: Response) {
     return res.status(200).send({message: 'put'})
   }
 
-  async delete(req: Request,res: Response): Promise<any> {
+  async delete(req: Request, res: Response) {
     return res.status(200).send({message: 'delete'}) 
   }
 }
