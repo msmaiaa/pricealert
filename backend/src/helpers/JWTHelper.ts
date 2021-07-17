c9c7057858a7b34970134c2172cf9cc477374950
msmaiaa <roxx77344@gmail.com>
2021-07-16 21:07:48 -0300

    (back)feat: create function to verify the user authentication

---
+++ backend/src/helpers/JWTHelper.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken'
+
+const verifyJWT = (req: any, res: Response, next: NextFunction) => {
+  const token = req.headers['x-access-token']
+  if (!token) return res.status(401).json({ message: 'No token provided.' });
+  
+  jwt.verify(token, `${process.env.JWT_SECRET}`, (err: any, decoded: any) => {
+    if (err) return res.status(500).json({ message: 'Failed to authenticate token.' });
+  
+    req.userid = decoded._id;
+    next();
+  });
+}
+
+export default verifyJWT
