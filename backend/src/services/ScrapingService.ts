9f8aef1cc1963e75f9dc6bcab64dee95f9a626c8
msmaiaa <roxx77344@gmail.com>
2021-07-18 01:56:10 -0300

    (back)feat: create ScrapingService, gather product info on first post and getProductName

--- backend/src/services/ScrapingService.ts
+++ backend/src/services/ScrapingService.ts
@@ -1,5 +1,21 @@
 import vanillaPuppeteer from 'puppeteer'
 import { Cluster } from 'puppeteer-cluster'
+import { ProductType } from '../repositories/ProductsRepository'
+import { addExtra } from "puppeteer-extra"
+import Stealth from "puppeteer-extra-plugin-stealth"
+import * as config from '../config/ScrapeConfigs.json'
+const puppeteer = addExtra(vanillaPuppeteer as any)
+puppeteer.use(Stealth())
+interface PuppeteerClusterParams {
+  page: vanillaPuppeteer.Page
+  data: {
+    prod: {
+      store: string,
+      url: string
+    }
+    userid: string
+  }
+}
 
 export default new class ScrapingService {
   supportedStores = ["kabum", "pichau", "terabyte"]
@@ -15,7 +31,57 @@ export default new class ScrapingService {
 
   }
 
-  async getManyProductsInfo (products: [String]) {
-    
+  //initial info gather when the user inserts a new product
+  async getManyProductsInfo (products: [string], userid: string): Promise<any> {
+    //: Promise<[ProductType]>
+    const productsWithStore = products.map((p) => {
+      return { store: this.detectStore(p), url: p }
+    })
+
+    const cluster: Cluster = await Cluster.launch({
+      puppeteer,
+      concurrency: Cluster.CONCURRENCY_CONTEXT,
+      maxConcurrency: 1,
+    });
+    for(const prod of productsWithStore) {
+      cluster.queue({prod, userid}, async ({page, data}: PuppeteerClusterParams): Promise<any> => {
+        const fullProduct = {
+          url: data.prod.url,
+          store: data.prod.store,
+          name: '',
+          price: 0,
+          imgUrl: ''
+        }
+        await page.goto(fullProduct.url)
+        console.log('after page goto')
+        fullProduct.name = await this.getProductName(fullProduct.store, page)
+        // fullProduct.imgUrl = await this.getProductImage(fullProduct.store, page)
+        // fullProduct.price = await this.getProductPrice(fullProduct.store, page)
+        console.log(fullProduct)
+      })
+    }
+
+    await cluster.idle();
+    await cluster.close();
+  }
+
+  async getProductName(store: string, page: vanillaPuppeteer.Page): Promise<string> {
+    const titleXpath: string = config.titleElement[store as 'kabum' | 'terabyte' | 'pichau'] 
+    await page.waitForXPath(titleXpath)
+    const titleElement = await page.$x(titleXpath)
+    const title = await page.evaluate(el => el.textContent, titleElement[0])
+    return Promise.resolve(title)
+  }
+
+  async getProductPrice(store: any, page: vanillaPuppeteer.Page): Promise<number> {
+    return Promise.resolve(1)
+  }
+
+  async getProductImage(store: any, page: vanillaPuppeteer.Page): Promise<string> {
+    return Promise.resolve('1')
+  }
+
+  async saveProductsInDatabase(products:[ProductType]) {
+
   }
 }
