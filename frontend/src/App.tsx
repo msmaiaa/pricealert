c7c930047c7fbcfd0c87953d93eb1c03539af4e1
msmaiaa <roxx77344@gmail.com>
2021-07-22 22:34:11 -0300

    (front)feat: add AuthProvider and AlertProvider to app root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import LoginPage from './pages/login/Login';
+import RegisterPage from './pages/register/Register';
+import HomePage from './pages/home/Home';
 
-function App() {
+import { history } from './helpers/history'
+import { Router, Switch, Route } from "react-router-dom";
+import ProtectedRoute from './helpers/ProtectedRoute'
+import Navbar from './components/navbar/Navbar';
+import { AuthProvider } from './Context/AuthContext'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-mui'
+
+const alertOptions = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
+
+function App(props: any) {
   return (
     <div className="App">
+      <AuthProvider>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Navbar/>
+          <Router history={history}>
+            <Switch>
+              <ProtectedRoute exact path="/" component={HomePage} />
+              <Route exact path="/login">
+                <LoginPage/>
+              </Route>
+              <Route exact path="/register">
+                <RegisterPage/>
+              </Route> 
+              <Route component={LoginPage}/>
+            </Switch>
+          </Router>
+        </AlertProvider>
+      </AuthProvider>
     </div>
   );
 }
 
-export default App;
+export default App
