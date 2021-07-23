fa90abbaf38a5b5ee36904bcd328210407f08df8
msmaiaa <roxx77344@gmail.com>
2021-07-22 22:31:17 -0300

    (front)feat: create basic navbar

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return(
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Navbar</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
