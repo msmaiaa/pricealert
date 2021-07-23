a10624279f3f75b54aa04e9004a59144dc46d68f
msmaiaa <roxx77344@gmail.com>
2021-07-23 00:51:06 -0300

    (front)feat: create Navbar component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,13 +1,66 @@
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import styled from 'styled-components'
+import { AuthContext } from '../../Context/AuthContext'
+
+const NavbarContainer = styled.nav`
+  height: 50px;
+  background-color: #4a148c;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0 15px;
+`
+
+const NavbarItems = styled.div`
+  display: flex;
+  justify-content: end;
+`
+
+const StyledLink = styled(Link)`
+  font-size: 16px;
+  font-weight: 400;
+  color: #fff;
+  &:not(:last-of-type) {
+    margin-right: 25px;
+  }
+  border: 1px solid white;
+  border-radius: 4px;
+  padding: 7px 10px;
+`
+
+const StyledA = styled.a`
+  font-size: 16px;
+  font-weight: 400;
+  color: #fff;
+  &:not(:last-of-type) {
+    margin-right: 25px;
+  }
+  border: 1px solid white;
+  border-radius: 4px;
+  padding: 7px 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 const Navbar = () => {
+  const { authenticated, handleLogout } = useContext(AuthContext)
+
   return(
-    <div>
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Navbar</a>
-        </div>
-      </nav>
-    </div>
+    <NavbarContainer>
+        {!authenticated ?
+        <NavbarItems>
+          <StyledLink to="/register">Register</StyledLink>
+          <StyledLink to="/login">Login</StyledLink>
+        </NavbarItems>
+        :
+        <NavbarItems>
+          <StyledA onClick={handleLogout}>Logout</StyledA>
+        </NavbarItems>
+        }
+    </NavbarContainer>
   )
 }
 
